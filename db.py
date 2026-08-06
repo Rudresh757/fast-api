7d2089d11d71c304e05fc6a5a883a9c85352c38a
rudreshdixit <rudresh.dixit757@gmail.com>
2026-08-06 20:17:31 +0530

    second commit

--- db.py
+++ db.py
@@ -1,7 +1,1 @@
-from sqlalchemy import create_engin
-
-MYSQL_USER = root
-MYSQL_PASSWORD = rudr 
-MYSQL_HOST = localhost
-MYSQL_PORT =    3306
-MYSQL_DATABASE = fast_api
+DBMS_URL=
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,3 +10,11 @@ def home():
 @app.get("/about")
 def about():
     return {"hello customer"}
+class Book(BaseModel):
+    book_name: str
+    book_author: str
+    book_price: float
+    book_serial_number: int
+
+@app.post("/book")
+def create_book(book:Book)
